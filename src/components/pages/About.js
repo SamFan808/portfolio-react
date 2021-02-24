68d38c03f5fef76dbe75e32d723fde54fd3378d4
Sam Fan <samisfan@msn.com>
2021-02-24 14:07:49 -0700

    first commit, file structure, and temp working files for reference

---
+++ src/components/pages/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <section class="container-fluid">
+        <section class="row-fluid">
+          <section class="col-sm-6 col-xs-12">
+            <section class="card">
+              <section class="card-body">
+                <h2>About Me</h2>
+                <hr />
+                <img
+                  src="Assets/images/profilepic_2.jpg"
+                  class="img-fluid"
+                  alt="profile picture"
+                />
+                <p>
+                  Sam was born on the island of O'ahu in the State of Hawaii. He
+                  was raised by native geese, known in Hawaiian as the Nene.
+                  Though the Hawaiian goose resembles the Canadian Goose, the
+                  Hawaiian goose does not prefer pineapple on their pizzas.
+                  Despite having been raised by geese, Sam does not currently
+                  show any traits or habits typical of Hawaiian geese. However,
+                  they would not refuse the addition of Canadian Bacon. Sam left
+                  the sunny shores of Hawaii for the snowy slopes of Colorado.
+                  He attended college for several years, and attained both
+                  undergraduate and graduate degrees. He is for the most part a
+                  fully functional human adult. He currently resides in Denver,
+                  Colorado and frequently sees several of the local Canadian
+                  Geese, and often will wave to them, bringing back fond
+                  memories of his childhood.
+                </p>
+              </section>
+            </section>
+          </section>
+        </section>
+      </section>
+    </div>
+  );
+}
+
+export default About;
