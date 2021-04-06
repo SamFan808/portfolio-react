c99bb3e8b983aa37eba1fc523af94068714f4e18
Sam Fan <samisfan@msn.com>
2021-04-06 17:20:32 -0600

    updated default home page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
     <Router>
       <div>
         <NavTabs />
-        <Route exact path="/" component={About} />
+        <Route exact path="/" component={Portfolio} />
+        <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
         <Footer />
